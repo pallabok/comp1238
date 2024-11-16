580c3a938e6108e09ef5ae5610955860eb31dd49
care <carrieinesitachoi@gmail.com>
2024-11-16 00:04:23 -0500

    Update lab11.sql

--- lab11.sql
+++ lab11.sql
@@ -19,3 +19,12 @@ FROM assignments
 WHERE due_date = '2024-10-08'
 
 --Question 5
+SELECT title, due_date
+FROM assignments
+WHERE due_date LIKE '2024-10%'
+
+--Question 6
+SELECT title, status, max (due_date) AS due_date
+FROM assignments
+WHERE status = 'Completed'
+ AND due_date <= '2024-11-15'
